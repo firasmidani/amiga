4a4976fcf8f199e98c73a520c87f76ea9ebb5826
Firas Said Midani <firasmidani@gmail.com>
2020-02-03 14:18:09 -0600

    passed initial QC for assmebleMappingData()

--- libs/aio.py
+++ libs/aio.py
@@ -36,7 +36,9 @@ import sys
 import numpy as np
 import pandas as pd
 import tabulate
+import string
 
+from libs import biolog_pm_layout as bpl
 
 def breakDownFilePath(filepath,copydirectory):
     '''
@@ -116,7 +118,8 @@ def parseCommand(config):
 
     # print default settings for select variables if prompted by user
     if args_dict['opd']:
-        msg = '\nDefault settings for select variables. You can adjust these values in libs/config.py. \n\n'
+        msg = '\nDefault settings for select variables. '
+        msg += 'You can adjust these values in libs/config.py. \n\n'
         msg += tidyDictPrint(config)
         sys.exit(msg)
         
@@ -607,7 +610,7 @@ def readPlateReaderData(filepath,interval,copydirectory,save=False):
 
     return df
 
-def parseWellLayout():
+def parseWellLayout(order_axis=1):
     '''
     Initializes a pandas.DataFrame where indices (row names) are well IDs (e.g. C8) and
         variables indicate row letter and column number.
@@ -622,7 +625,7 @@ def parseWellLayout():
     '''
 
     # initialize rows = ['A',...,'H'] and cols = [1,...,12]
-    rows = list(string.ascii.uppercase[0:8])
+    rows = list(string.ascii_uppercase[0:8])
     cols = list(str(ii) for ii in range(1,13))
 
     list_wells = []
@@ -698,26 +701,7 @@ def readPlateReaderFolder(filename,directory,config,interval_dict={},save=False,
     return df_dict
 
 
-def extractFromMeta(filebase,meta_df):
-    '''
-    Checks if a data file has been described in meta.txt dataframe.
-
-    Args:
-        filebase (str): Plate_ID (i.e. file name)
-        meta_df (pandas.DataFrame): first
-
-    Returns:
-        meta_info (pd.Series or None)
-    '''
-
-    if filebase in meta_df.Plate_ID.values:
-        meta_info =i meta_df.Plate_ID==filebase]
-    else:
-        meta_info = None
-
-    return meta_info 
-
-def assemblePath(directory,filename,extension=''):
+def assemblePath(directory,filebase,extension=''):
     '''
     Stitches a full path to a file.
 
@@ -752,63 +736,264 @@ def checkPlateIdColumn(df,filebase):
         df.loc[:,'Plate_ID'] = [filebase]*df.shape[0]
 
     return df
-        
+
+
+def isBiologFromMeta(series):
+    '''
+    Identifies if a data file is a Biolog plate based on its meta-data.
+
+    Args:
+        ser (pandas.Series)
+
+    Returns:
+        (boolean)
+    '''
+
+    if 'PM' not in series.keys():
+        return False
+    elif grabValueFromDf(series,'PM') in range(1,7): 
+        return True
+    else:
+        return False
+
+
+def isBiologFromName(filebase):
+    '''
+    Determines if file name nomenclature indicates Biolog PM plate. Biolog PM nomenclature is
+        {Isolate Name}_PM{PM plate number}-{Replicate number}.
+
+    Args:
+        filebase (str)
+
+    Returns:
+        (boolean) 
+    '''
+
+
+def initMappingDf(filebase,well_ids):
+    '''
+    '''
+
+    data = [filebase]*len(well_ids)
+
+    df_mapping = pd.DataFrame(index=well_ids,columns=['Plate_ID'],data=data)
+
+    return df_mapping
+
+
+def grabValueFromDf(df,key,fillna=None):
+    '''
+    Grabs the first value in a specific column in either a data frme or a series.
+
+    Args:
+        df (pandas.DataFrame or pandas.Series)
+        key (str): column key
+        fillna (str or None): if value not found, should replace with this argument
+
+    Returns:
+        value (data type is flexible)
+    '''
+
+    if key in df.keys():
+        return df.loc[:,key].iloc[0]
+    else:
+        return fillna
+
+
+def parseBiologLayout():
+    '''
+    Initializes a pandas.DataFrame that maps the location (well and plate number)
+        of each sugar in BIOLOG PM plates.
+
+    Retruns:
+        df (pandas.DataFrame)
+    '''
+
+    biolog_layout = [bpl.Carbon1,bpl.Carbon2,bpl.PhosphorusAndSulfur]
+    biolog_layout += [bpl.PeptideNitrogen1,bpl.PeptideNitrogen2,bpl.PeptideNitrogen3]
+
+    index = [str(ii) for ii in range(1,7)]
+    keys = parseWellLayout(order_axis=0).index
+
+    biolog_layout_df = pd.DataFrame(biolog_layout,index=index,columns=keys).T
+    biolog_layout_df.columns.name = 'PM'
+
+    return biolog_layout_df 
+
+def initSubstrateDf(pmn):
+    '''
+    Initializes a pandas DataFrame for substrates corresponding to a specific BIOLOG PM plate.
+
+    Args:
+        pmn (str or int or float): Phenotypic Microarray plate number, must be between [1,7)
+    '''
+
+    sr = parseBiologLayout().loc[:,str(int(pmn))]
+    df = pd.DataFrame(sr)
+    df.columns = ['Substrate']
+
+    return df
+
+
+def initKeyFromMeta(series,well_ids):
+    '''
+    Initializes a mapping data frame using data derived from meta.txt. Here, it will reproduce
+        a pandas.Series to match the number of wells in corresponding data file.
+
+    Args:
+        sr (pandas.Series): no size limit
+        well_ids (list): list of variable data type
+
+    Returns:
+        df_meta (pandas.DataFrame): # rows = len(well_ids), # cols = df.shape[1]
+    '''
+
+    df_meta = pd.concat([series]*len(well_ids))
+    df_meta.index = well_ids
+
+    return df_meta
+
+
+def expandBiologMetaData(sr):
+    '''
+    Builds a pandas.DataFrame that combines file-specific meta-data with info derived from
+        BIOLOG PM substrate lay-out. Datafrmae will include columns for Plate ID, Isolate 
+        name, PM number, and Replicate number. 
+
+    Args:
+        sr (pandas.Series): describes meta-data for a specific file
+
+    Returns:
+        df (pandas.DataFrame)
+    '''
+
+    pmn = grabValueFromDf(sr,'PM')  # PM plate number
+    df_substrates = initSubstrateDf(pmn)  # mapping of wells to substrates
+    df_meta = initKeyFromMeta(sr,df_substrates.index)
+    df = df_meta.join(df_substrates)
+
+    return df
+
+
+def initBiologPlateKey(plate_id,simple=False):
+    '''
+    Initiailzes a mapping dataframe using only Plate ID. Dataframe will have columns
+        for Plate_ID, Isolate name, PM number, and Replicate number.
+
+    Args:
+        plate_id (str): data file name (i.e. filebase)
+        simple (boolean): whether to include replicate number in mapping dataframe or not
+
+    Returns:
+        df_mapping (pandas.DataFrame)
+    '''
+
+    list_keys = ['Plate_ID','Isolate','PM','Replicate']
+    isolate,pmn,rep = parsePlateName(plate_id,simple = False)
+    df_meta = pd.Dataframe(indx=list_keys,data=[plate_id,pmn,rep]).T
+    df = expandBiologMetaData(df_meta)
+
+    return df
+
+
+def parsePlateName(plate_id,simple=False):
+    '''
+    Deconstructs a file name to identify Isolate name, PM plate number, and Replicate number.
+
+    Args:
+        plate_id (str)
+        simple (boolean): whether to return Replicate number
+
+    Returns:
+        isolate (str)
+        pmn (int)
+        rep (int)
+    '''
+
+    isolate = str(plate_name.split('_')[0])
+    pmn = int(plate_name.split('PM')[1][0])
+    rep = [int(plate_name.split('-')[-1]) if '-' in plate_name else 1][0]
+
+    if simple:
+        return isolate,pmns
+    else:
+        return isolate,pmn,rep
+
+
 def assembleMappingData(data,mapping_path,meta_path):
     '''
+    Creates a master mapping file (or dictionary ?) for all data files in the input argument.
+        For each data file, in this particular order, it will first (1) check if an individual
+        mapping file exists, (2) if not, check if relevant meta-data is provided in meta.txt
+        file, (3) if not, infer if plate is a BIOLOG PM based on its file name, and (4) if all
+        fail, create a minimalist mapping file. 
+
+    Args:
+        data (dictionary): keys are file names (i.e. filebases or Plate IDs) and values are
+            pandas DataFrames where index column (row names) are well IDs.
+        mapping_path (str): path to the mapping folder.
+        meta_path (str): path to the mapping file.
+
+    Returns:
+        df_mapping_dict (dictionary): keys are file names and values are mapping files. 
     '''
+
+    df_mapping_dict = {}
 
     # list all data files to be analyed
     list_filebases = data.keys()
 
     # list all potential mapping file paths
-    list_mapping_files = [assmeblePath(mapping_path,ii,'.txt') for ii in list_filebases]
+    list_mapping_files = [assemblePath(mapping_path,ii,'.txt') for ii in list_filebases]
 
     # read meta.txt and list all plates described by it
     meta_df, meta_df_plates = checkMetaText(meta_path)   
 
+    msg = ''
     # assemble mapping for one data file at a time
-    for filebase,mapping_file_path in (list_filebases,list_mapping_files):
+    for filebase,mapping_file_path in zip(list_filebases,list_mapping_files):
+
+        # what are the row names from the original data file 
+        well_ids = data[filebase].columns[1:]  # this may no be A1 ... H12, but most ofen will be
 
         # user provided a mapping file that corresponds to this data file (filebase)
         if os.path.exists(mapping_file_path):
 
             df_mapping = pd.read_csv(mapping_file_path,sep='\t',header=0,index_col=None)   
             df_mapping = checkPlateIdColumn(df_mapping,filebase) # makes sure Plate_ID is a column
-            msg = '{}: Reading {}'.format(filebase,mapping_file_path)
+            msg += '{:.<24}Reading {}.\n'.format(filebase,mapping_file_path)
 
+        # see if file is described in meta.txt 
         elif filebase in meta_df_plates:
 
-            meta_info = extract 
+            meta_info = meta_df[meta_df.Plate_ID==filebase]
+            msg += '{:.<24}Found meta-data in meta.txt '.format(filebase)
 
-extractFromMeta(filebase,df_meta_,meta_df):
+            biolog = isBiologFromMeta(meta_info)
 
+            if biolog:
+                df_mapping = expandBiologMetaData(meta_info)
+                msg += '& seems to be a BIOLOG PM plate.\n'
+            else:
+                df_mapping = initKeyFromMeta(meta_info,well_ids)
+                msg += '& does not seem to be a BIOLOG PM plate.\n'
 
-        # what are the row names from the original data file 
-        well_ids = data[filebase].columns[1:]  # this may no be A1 ... H12, but most ofen will be
+        elif isBiologFromName(filebase):
 
-        # check if filebase is describe in meta-data 
-        meta_info = extractFromMeta(filebase,meta_df_plates,meta_df):
+            df_mapping = initBiologPlateKey(meta_info)
+            msg += '{:.<24}Did not find mapping file or meta-data '.format(filebase)
+            msg += 'BUT seems to be a BIOLOG PM plate.\n'
 
+        else:
+            df_mapping = initMappingDf(filebase,well_ids) 
+            msg += '{:.<24}Did not find mapping file or meta-data '.format(filebase)
+            msg += '& does not seem to be a BIOLOG PM plate.\n'
 
-        mapping[filebase] = pipeline.smartMapping(filebase,mapping_path,meta_info)
+        df_mapping_dict[filebase] = df_mapping
 
+    #df_mapping = df_mapping.reset_index(drop=False)
+    print(msg) 
 
-def smartMapping(filebase,mapping_path,meta_info):
-    '''
-    '''
-
-    meta_df,meta_df_plates = checkMetaText(meta_path)
-
-
-
-    # user describe data file in meta.txt
-    elif meta_pass is not None:
-
-        metadata = meta_pass
-
-
-    #elif filebase in df_meta_
-        
 
 def printDirectoryContents(directory,sort=True,tab=True):
     '''
