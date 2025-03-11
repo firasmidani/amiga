3882eae8bcf778d08ea198d942e38c1519906fa1
Firas Midani <firasmidani@gmail.com>
2025-03-10 19:11:09 -0500

    Bumped GPy to avoid issue #1079

--- amiga/libs/test.py
+++ amiga/libs/test.py
@@ -35,8 +35,6 @@ from .params import articulateParameters, prettyifyParameterReport
 from .plot import addRealPlotLine, addMVNPlotLine, setAxesLabels
 from .plot import dynamicWindowAdjustment, plotDeltaOD, savePlotWithLegends
 
-pd.set_option('future.no_silent_downcasting', True)
-
 
 class HypothesisTest:
 
